7bf29c0f7acb5a8ee972d1264b2d077275fde317
Gabriel Fernella Garcia dos Santos <GabrielFernella@users.noreply.github.com>
2020-05-13 21:27:29 -0300

    update multer

--- API-Gobarber2/src/app/controllers/FileController.js
+++ API-Gobarber2/src/app/controllers/FileController.js
@@ -1,1 +1,29 @@
 //Controller destinado a salvar as refeências do arquivo no banco de dados 
+import File from '../models/File';
+
+class FileController {
+    async store(req, res) {
+        const { originalname: name, filename: path } = req.file;
+
+        const file = await File.create({
+            name,
+            path
+        })
+        
+        return res.json(file);
+    }
+}
+
+export default new FileController(); 
+
+/*
+    Precisa existir uma tabela no banco de dados para guardar as referencias
+    npx sequelize migration:create --name=create-files
+    
+    Edite e rode 
+    npx sequelize  db:migrate
+
+
+
+    avatar usuário 2 , quase metade 
+*/
